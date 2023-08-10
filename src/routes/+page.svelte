2b32dd23d24b0941f10282c03dbdea5163907d22
tarikjaber <tariknjaber@gmail.com>
2023-08-10 11:53:45 -0400

    Finish basic functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,100 @@
+<script lang="ts">
+	import { parseTasks } from "../utils";
+	import type { Task } from "../utils/types";
+	
+	let playing = false;
+	let textarea: HTMLTextAreaElement;
+	let intervalId: number;
+	let timeLeft = 0;
+	let tasks: Task[] = [];
+	let failed = false;
+	let currentTaskIndex = 0;
+	let startTime: number;
+	$: timeString = (Math.floor(timeLeft / 60)).toString().padStart(2, '0') + ':' + (timeLeft % 60).toString().padStart(2, '0');
+
+	function clearAll() {
+		textarea.value = '';
+		textarea.focus();
+		tasks = [];
+		
+		playing = false;
+		if (intervalId) {
+			window.clearInterval(intervalId);
+		}
+	}
+
+	function onClear() {
+		console.log('on clear')
+		clearAll();
+	}
+
+	function onDone() {
+		console.log('on done')
+		currentTaskIndex++;
+		if (currentTaskIndex >= tasks.length) {
+			console.log("running clear all")
+			clearAll();
+			return;
+		}
+		startTime = Date.now();
+	}
+
+	function onStart() {	
+		console.log('on start')
+		const tasksInputValue = textarea.value;
+		failed = false;
+		currentTaskIndex = 0;
+		tasks = parseTasks(tasksInputValue);
+
+		if (tasks.length == 0) {
+			alert('No tasks found');
+			return;
+		}
+		let newTextAreaValue = '';
+		for (const task of tasks) {
+			newTextAreaValue += task.name + ' ' + task.time / 60 + '\n';
+		}
+		textarea.value = newTextAreaValue;
+		console.log(tasks);
+		playing = true;
+
+		startTime = Date.now();
+
+		intervalId = window.setInterval(() => {
+			console.log("interval called")
+			const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+			timeLeft = tasks[currentTaskIndex]?.time - elapsedTime;
+			console.log(timeLeft);
+
+			if (timeLeft <= 0) {
+				failed = true;
+				clearAll();
+			}
+		}, 1000);
+	}
+</script>
+
 <div class="vertical">
 	<div class="body">
-		<h1 class="h1">Task Timer</h1>
-		<h2 class="h2">00:00</h2>
-		<textarea class="textarea" rows="16" placeholder="Enter tasks..." />
+		{#if playing}
+			<h1 class="h1">{tasks[currentTaskIndex]?.name}</h1>
+			<h1 class="h2">{timeString}</h1>
+		{:else}
+			{#if failed}
+				<h1 class="h1 text-red-600">Failed</h1>
+			{:else}
+				<h1 class="h1">Task Timer</h1>
+			{/if}
+			<h2 class="h2">00:00</h2>
+		{/if}
+		<textarea class="textarea" rows="16" placeholder="Enter tasks..." bind:this={textarea} />
 		<div class="buttons">
-			<button type="button" class="btn btn-xl variant-filled-secondary">Clear</button>
-			<button type="button" class="btn btn-xl variant-filled-tertiary">Done</button>
-			<button type="button" class="btn btn-xl variant-filled-primary">Start</button>
+			<button type="button" on:click={onClear} class="btn btn-xl variant-filled-secondary">Clear</button>
+			{#if playing}
+				<button type="button" on:click={onDone} class="btn btn-xl variant-filled-tertiary">Done</button>
+			{:else}
+				<button type="button" on:click={onStart} class="btn btn-xl variant-filled-primary">Start</button>
+			{/if}
 		</div>
 	</div>
 </div>
@@ -54,5 +142,4 @@
 		justify-content: center;
 		margin-top: 10px;
 	}	
-
 </style>
